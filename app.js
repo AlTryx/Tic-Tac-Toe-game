029f93c989273266bc830950970cdc060be1c611
Aleksandar Stanchev <altryx15@gmail.com>
2024-05-26 22:59:38 +0300

    I've made the 9 divs for the game

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const gameBoard = document.querySelector('#gameboard')
 const infoDisplay = document.querySelector('#info')
+const startCells = [
+    "", "", "",
+    "", "", "",
+    "", "", "",
+]
 
+infoDisplay.textContent = "Circle goes first"
+
+function createBoard() {
+    startCells.forEach((cell, index) => {
+       const cellElement = document.createElement("div")
+       cellElement.classList.add("square")
+       gameBoard.append(cellElement)
+    })
+}
+createBoard()
 
